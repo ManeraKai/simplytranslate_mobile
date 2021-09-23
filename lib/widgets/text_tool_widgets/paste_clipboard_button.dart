be13c5cd5112a63fc696eac91ebee3c9cf9f18af
Esmail Almaleeh <hazem_almaleeh1@hotmail.com>
2021-09-23 11:47:35 +0300

    Fixed #78

--- lib/widgets/text_tool_widgets/paste_clipboard_button.dart
+++ lib/widgets/text_tool_widgets/paste_clipboard_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:simplytranslate/widgets/keyboard_visibility_widget.dart';
 import '../../data.dart';
 
 class PasteClipboardButton extends StatelessWidget {
@@ -15,60 +16,71 @@ class PasteClipboardButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.topRight,
-      child: IconButton(
-        color: theme == Brightness.dark ? null : greenColor,
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        onPressed: isClipboardEmpty == true
-            ? null
-            : () {
-                Clipboard.getData(Clipboard.kTextPlain).then((value) async {
-                  FocusScope.of(context).unfocus();
+      child: KeyboardVisibilityBuilder(
+        builder: (context, _, isKeyboardVisible) => IconButton(
+          color: theme == Brightness.dark ? null : greenColor,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onPressed: isClipboardEmpty == true
+              ? null
+              : () {
+                  Clipboard.getData(Clipboard.kTextPlain).then((value) async {
+                    FocusScope.of(context).unfocus();
 
-                  if (value != null) {
-                    final valueString = value.text.toString();
-                    if (translationInputController.text == '') {
-                      await Future.delayed(
-                          const Duration(milliseconds: 1), () => "1");
-                      FocusScope.of(context).requestFocus(focus);
-                      setStateParent(() {
-                        translationInput = valueString;
-                        translationInputController.text = valueString;
-                        translationLength =
-                            translationInputController.text.length;
-                      });
-                    } else {
-                      final beforePasteSelection =
-                          translationInputController.selection.baseOffset;
-                      final newText;
-                      if (beforePasteSelection == -1)
-                        newText = translationInputController.text + valueString;
-                      else
-                        newText = translationInputController.text
-                                .substring(0, beforePasteSelection) +
-                            valueString +
-                            translationInputController.text.substring(
-                                beforePasteSelection,
-                                translationInputController.text.length);
+                    if (value != null) {
+                      final valueString = value.text.toString();
+                      if (translationInputController.text == '') {
+                        await Future.delayed(
+                            const Duration(milliseconds: 1), () => "1");
+                        FocusScope.of(context).requestFocus(focus);
+                        setStateParent(() {
+                          translationInput = valueString;
+                          translationInputController.text = valueString;
+                          translationLength =
+                              translationInputController.text.length;
+                        });
+                      } else {
+                        final beforePasteSelection =
+                            translationInputController.selection.baseOffset;
+                        final newText;
+                        if (beforePasteSelection == -1)
+                          newText =
+                              translationInputController.text + valueString;
+                        else
+                          newText = translationInputController.text
+                                  .substring(0, beforePasteSelection) +
+                              valueString +
+                              translationInputController.text.substring(
+                                  beforePasteSelection,
+                                  translationInputController.text.length);
 
-                      await Future.delayed(
-                          const Duration(milliseconds: 1), () => "1");
-                      FocusScope.of(context).requestFocus(focus);
+                        await Future.delayed(
+                            const Duration(milliseconds: 1), () => "1");
+                        FocusScope.of(context).requestFocus(focus);
 
-                      setStateParent(() {
-                        translationInput = newText;
-                        translationInputController.text = newText;
-                        translationInputController.selection =
-                            TextSelection.collapsed(
-                                offset:
-                                    beforePasteSelection + valueString.length);
-                      });
+                        setStateParent(() {
+                          translationInput = newText;
+                          translationInputController.text = newText;
+                          if (isKeyboardVisible) {
+                            translationInputController.selection =
+                                TextSelection.collapsed(
+                                    offset: beforePasteSelection +
+                                        valueString.length);
+                          } else {
+                            translationInputController.selection =
+                                TextSelection.collapsed(
+                                    offset:
+                                        translationInputController.text.length);
+                          }
+                        });
+                      }
+                      translationLength =
+                          translationInputController.text.length;
                     }
-                    translationLength = translationInputController.text.length;
-                  }
-                });
-              },
-        icon: Icon(Icons.paste),
+                  });
+                },
+          icon: Icon(Icons.paste),
+        ),
       ),
     );
   }
