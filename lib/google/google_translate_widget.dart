bb674583e349331fba9aa5acf4303809fe70a43a
ManeraKai <hazem_almaleeh1@hotmail.com>
2021-10-13 22:23:44 +0300

    Fixing context bug #61

--- lib/google/google_translate_widget.dart
+++ lib/google/google_translate_widget.dart
@@ -12,8 +12,6 @@ import 'widgets/lang_selector/from_lang.dart';
 import 'widgets/lang_selector/to_lang.dart';
 import 'widgets/lang_selector/switch_lang.dart';
 
-var _scrollController = ScrollController();
-
 class GoogleTranslate extends StatefulWidget {
   final setStateParent;
   const GoogleTranslate({
@@ -25,84 +23,65 @@ class GoogleTranslate extends StatefulWidget {
   State<GoogleTranslate> createState() => _GoogleTranslateState();
 }
 
-var _isBefore = false;
-
 class _GoogleTranslateState extends State<GoogleTranslate> {
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    if (MediaQuery.of(context).orientation == Orientation.landscape) {
-      _isBefore = true;
-    } else if (MediaQuery.of(context).orientation == Orientation.portrait &&
-        _isBefore) {
-      _scrollController.jumpTo(0);
-      _isBefore = false;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SingleChildScrollView(
-          controller: _scrollController,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GoogleFromLang(setStateOverlord: widget.setStateParent),
-                      GoogleSwitchLang(
-                        setStateParent: widget.setStateParent,
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GoogleFromLang(setStateOverlord: widget.setStateParent),
+                    GoogleSwitchLang(
+                      setStateParent: widget.setStateParent,
+                    ),
+                    GoogleToLang(
+                      setStateOverlord: widget.setStateParent,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                GoogleTranslationInput(
+                  setStateParent: widget.setStateParent,
+                ),
+                const SizedBox(height: 10),
+                GoogleTranslationOutput(),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    KeyboardVisibilityBuilder(
+                      builder: (context, child, isKeyboardVisible) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          loading
+                              ? Container(
+                                  width: MediaQuery.of(context).size.width - 20,
+                                  child: LinearProgressIndicator(),
+                                )
+                              : const SizedBox.shrink(),
+                          loading
+                              ? GoogleCancelTranslationButton(
+                                  setStateParent: widget.setStateParent)
+                              : !isKeyboardVisible
+                                  ? GoogleTranslateButton(
+                                      setStateParent: widget.setStateParent,
+                                      contextParent: context,
+                                    )
+                                  : const SizedBox.shrink(),
+                        ],
                       ),
-                      GoogleToLang(
-                        setStateOverlord: widget.setStateParent,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  GoogleTranslationInput(
-                    setStateParent: widget.setStateParent,
-                  ),
-                  const SizedBox(height: 10),
-                  GoogleTranslationOutput(),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      KeyboardVisibilityBuilder(
-                        builder: (context, child, isKeyboardVisible) => Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            loading
-                                ? Container(
-                                    width:
-                                        MediaQuery.of(context).size.width - 20,
-                                    child: LinearProgressIndicator(),
-                                  )
-                                : const SizedBox.shrink(),
-                            loading
-                                ? GoogleCancelTranslationButton(
-                                    setStateParent: widget.setStateParent)
-                                : !isKeyboardVisible
-                                    ? GoogleTranslateButton(
-                                        setStateParent: widget.setStateParent,
-                                        contextParent: context,
-                                      )
-                                    : const SizedBox.shrink(),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+              ],
             ),
           ),
         ),
