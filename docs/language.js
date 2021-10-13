e34909c2f1e0a89ddba06908d747dc025956144b
ManeraKai <hazem_almaleeh1@hotmail.com>
2021-10-13 15:08:28 +0300

    Fixed font problem for polish #113

--- docs/language.js
+++ docs/language.js
@@ -2,5 +2,4 @@ if (navigator.language.substring(0, 2) == 'pl') {
     console.log('redirecting to index_pl.html');
     if (!window.location.href.endsWith('index_pl.html'))
         window.location.replace("./index_pl.html");
-
 }
