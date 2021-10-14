001fc9a1f9ac19fa905e0b2ca41ea6343a738da7
ManeraKai <hazem_almaleeh1@hotmail.com>
2021-10-14 16:58:23 +0300

    Added alefvanoon as a persian translator

--- docs/language.js
+++ docs/language.js
@@ -1,11 +1,11 @@
 if (navigator.language.substring(0, 2) == 'pl') {
     console.log('redirecting to index_pl.html');
-    if (!window.location.href.endsWith('index_pl.html'))
+    if (window.location.href.endsWith('index.html'))
     window.location.replace("./index_pl.html");
 }
 else if (navigator.language.substring(0, 2) == 'ar') {
     console.log('redirecting to index_ar.html');
-    if (!window.location.href.endsWith('index_ar.html'))
+    if (window.location.href.endsWith('index.html'))
     window.location.replace("./index_ar.html");
 }
 
