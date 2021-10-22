4c0dd2347a37894b6f10e86221a3c39cace589de
ManeraKai <manerakai@protonmail.com>
2021-10-22 12:06:29 +0300

    fixed bug in website

--- docs/language.js
+++ docs/language.js
@@ -1,11 +1,11 @@
 if (navigator.language.substring(0, 2) == 'pl') {
-    console.log('redirecting to pl.html');
-    if (window.location.href.endsWith('index.html'))
+    // console.log('redirecting to pl.html');
+    if (window.location.href.endsWith('index.html') || 'simplytranslate_mobile/')
     window.location.replace("./pl.html");
 }
 else if (navigator.language.substring(0, 2) == 'ar') {
-    console.log('redirecting to ar.html');
-    if (window.location.href.endsWith('index.html'))
+    // console.log('redirecting to ar.html');
+    if (window.location.href.endsWith('index.html') || 'simplytranslate_mobile/')
     window.location.replace("./ar.html");
 }
 
