6fba54aa5165449fe3aeeceec0d8f0174fd9bb44
ManeraKai <manerakai@protonmail.com>
2021-10-22 12:08:19 +0300

    Tweaked website

--- docs/language.js
+++ docs/language.js
@@ -1,11 +1,14 @@
 if (navigator.language.substring(0, 2) == 'pl') {
     // console.log('redirecting to pl.html');
-    if (window.location.href.endsWith('index.html') || 'simplytranslate_mobile/')
-    window.location.replace("./pl.html");
+    if (window.location.href.endsWith('index.html'))
+        window.location.replace("./pl.html");
+    else if (window.location.href.endsWith('simplytranslate_mobile/'))
+        window.location.replace("./pl");
 }
 else if (navigator.language.substring(0, 2) == 'ar') {
-    // console.log('redirecting to ar.html');
-    if (window.location.href.endsWith('index.html') || 'simplytranslate_mobile/')
-    window.location.replace("./ar.html");
+    if (window.location.href.endsWith('index.html'))
+        window.location.replace("./ar.html");
+    else if (window.location.href.endsWith('simplytranslate_mobile/'))
+        window.location.replace("./ar");
 }
 
