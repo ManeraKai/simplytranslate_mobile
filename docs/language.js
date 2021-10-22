cce550a9a8f2c05311e15cc00fc1e5da204d1d2a
ManeraKai <manerakai@protonmail.com>
2021-10-22 12:01:44 +0300

    changed lang page names in website

--- docs/language.js
+++ docs/language.js
@@ -1,11 +1,11 @@
 if (navigator.language.substring(0, 2) == 'pl') {
-    console.log('redirecting to index_pl.html');
+    console.log('redirecting to pl.html');
     if (window.location.href.endsWith('index.html'))
-    window.location.replace("./index_pl.html");
+    window.location.replace("./pl.html");
 }
 else if (navigator.language.substring(0, 2) == 'ar') {
-    console.log('redirecting to index_ar.html');
+    console.log('redirecting to ar.html');
     if (window.location.href.endsWith('index.html'))
-    window.location.replace("./index_ar.html");
+    window.location.replace("./ar.html");
 }
 
