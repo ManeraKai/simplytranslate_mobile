bb2ae94812d5fe51d78b8369d698f822f8a0d9e9
ManeraKai <manerakai@protonmail.com>
2021-10-25 17:20:35 +0300

    changed text selection toolbar color. Added bottom padding #150

--- lib/google/screens/maximized/maximized.dart
+++ lib/google/screens/maximized/maximized.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import './widgets/tts_output.dart';
 import '/data.dart';
@@ -35,7 +36,7 @@ class _MaximizedScreenState extends State<MaximizedScreen> {
           ),
           borderRadius: BorderRadius.circular(2),
         ),
-        margin: const EdgeInsets.only(top: 30, left: 10, right: 10, bottom: 10),
+        margin: const EdgeInsets.symmetric(vertical: 40,horizontal: 10),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -46,6 +47,7 @@ class _MaximizedScreenState extends State<MaximizedScreen> {
                       const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
                   child: SelectableText(
                     googleOutput,
+                    selectionControls: _MyMaterialTextSelectionControls(),
                     style: TextStyle(fontSize: outputFontSize),
                   ),
                 ),
@@ -83,6 +85,185 @@ class _MaximizedScreenState extends State<MaximizedScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+var _isVisible = true;
+
+class _MyMaterialTextSelectionControls extends MaterialTextSelectionControls {
+  // Padding between the toolbar and the anchor.
+  static const double _kToolbarContentDistanceBelow = 20.0;
+  static const double _kToolbarContentDistance = 8.0;
+
+  /// Builder for material-style copy/paste text selection toolbar.
+  @override
+  Widget buildToolbar(
+    BuildContext context,
+    Rect globalEditableRegion,
+    double textLineHeight,
+    Offset selectionMidpoint,
+    List<TextSelectionPoint> endpoints,
+    TextSelectionDelegate delegate,
+    ClipboardStatusNotifier clipboardStatus,
+    Offset? lastSecondaryTapDownPosition,
+  ) {
+    final TextSelectionPoint startSelectionPoint = endpoints[0];
+    final TextSelectionPoint endSelectionPoint =
+        endpoints.length > 1 ? endpoints[1] : endpoints[0];
+
+    final Offset anchorAbove = Offset(
+      globalEditableRegion.left + selectionMidpoint.dx,
+      globalEditableRegion.top +
+          startSelectionPoint.point.dy -
+          textLineHeight -
+          _kToolbarContentDistance,
+    );
+
+    final Offset anchorBelow = Offset(
+      globalEditableRegion.left + selectionMidpoint.dx,
+      globalEditableRegion.top +
+          endSelectionPoint.point.dy +
+          _kToolbarContentDistanceBelow,
+    );
+    _isVisible = () {
+      if (MediaQuery.of(context).orientation == Orientation.portrait) {
+        if (startSelectionPoint.point.dy < 20) return false;
+        if (startSelectionPoint.point.dy > textFieldHeight + 65) return false;
+      }
+      return true;
+    }();
+    return _MyTextSelectionToolbar(
+      anchorAbove: anchorAbove,
+      anchorBelow: anchorBelow,
+      clipboardStatus: clipboardStatus,
+      handleCopy: canCopy(delegate)
+          ? () => handleCopy(delegate, clipboardStatus)
+          : () {},
+      handleCut: canCut(delegate) ? () => handleCut(delegate) : () {},
+      handlePaste: canPaste(delegate)
+          ? () async {
+              isFirst = true;
+              handlePaste(delegate);
+            }
+          : () {},
+      handleSelectAll:
+          canSelectAll(delegate) ? () => handleSelectAll(delegate) : null,
+    );
+  }
+}
+
+class _TextSelectionToolbarItemData {
+  const _TextSelectionToolbarItemData({
+    required this.label,
+    required this.onPressed,
+  });
+  final String label;
+  final VoidCallback onPressed;
+}
+
+class _MyTextSelectionToolbar extends StatefulWidget {
+  const _MyTextSelectionToolbar({
+    Key? key,
+    required this.anchorAbove,
+    required this.anchorBelow,
+    required this.clipboardStatus,
+    required this.handleCopy,
+    required this.handleCut,
+    required this.handlePaste,
+    required this.handleSelectAll,
+  }) : super(key: key);
+
+  final Offset anchorAbove;
+  final Offset anchorBelow;
+  final ClipboardStatusNotifier clipboardStatus;
+  final VoidCallback handleCopy;
+  final VoidCallback handleCut;
+  final VoidCallback handlePaste;
+  final VoidCallback? handleSelectAll;
+
+  @override
+  _MyTextSelectionToolbarState createState() => _MyTextSelectionToolbarState();
+}
+
+class _MyTextSelectionToolbarState extends State<_MyTextSelectionToolbar> {
+  void _onChangedClipboardStatus() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.clipboardStatus.addListener(_onChangedClipboardStatus);
+    widget.clipboardStatus.update();
+  }
+
+  @override
+  void didUpdateWidget(_MyTextSelectionToolbar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.clipboardStatus != oldWidget.clipboardStatus) {
+      widget.clipboardStatus.addListener(_onChangedClipboardStatus);
+      oldWidget.clipboardStatus.removeListener(_onChangedClipboardStatus);
+    }
+    widget.clipboardStatus.update();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (!widget.clipboardStatus.disposed) {
+      widget.clipboardStatus.removeListener(_onChangedClipboardStatus);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMaterialLocalizations(context));
+    final MaterialLocalizations localizations =
+        MaterialLocalizations.of(context);
+
+    final List<_TextSelectionToolbarItemData> itemDatas =
+        <_TextSelectionToolbarItemData>[
+      _TextSelectionToolbarItemData(
+        label: localizations.cutButtonLabel,
+        onPressed: widget.handleCut,
+      ),
+      _TextSelectionToolbarItemData(
+        label: localizations.copyButtonLabel,
+        onPressed: widget.handleCopy,
+      ),
+      if (widget.clipboardStatus.value == ClipboardStatus.pasteable)
+        _TextSelectionToolbarItemData(
+          label: localizations.pasteButtonLabel,
+          onPressed: widget.handlePaste,
+        ),
+      if (widget.handleSelectAll != null)
+        _TextSelectionToolbarItemData(
+          label: localizations.selectAllButtonLabel,
+          onPressed: widget.handleSelectAll!,
+        ),
+    ];
+
+    int childIndex = 0;
+    return TextSelectionToolbar(
+      anchorAbove: widget.anchorAbove,
+      anchorBelow: widget.anchorBelow,
+      toolbarBuilder: (BuildContext context, Widget child) {
+        if (_isVisible)
+          return Container(
+            color: theme == Brightness.dark ? secondgreyColor : greenColor,
+            child: child,
+          );
+        return SizedBox.shrink();
+      },
+      children: itemDatas.map((_TextSelectionToolbarItemData itemData) {
+        return TextSelectionToolbarTextButton(
+          padding: TextSelectionToolbarTextButton.getPadding(
+              childIndex++, itemDatas.length),
+          onPressed: itemData.onPressed,
+          child: Text(itemData.label, style: TextStyle(color: Colors.white)),
+        );
+      }).toList(),
     );
   }
 }
