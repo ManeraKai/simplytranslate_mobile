d196599b6f041cf90f30a0a63466e3d431b179fc
ManeraKai <manerakai@protonmail.com>
2022-03-09 00:39:04 +0300

    Updated and fixed bugs in the website #113

--- docs/langs_list.js
+++ docs/langs_list.js
@@ -1,1 +1,1 @@
-const langs_list = ['ar', 'de', 'el', 'en', 'eo', 'es', 'fa', 'fi', 'fr', 'it', 'ml', 'nb_NO', 'pl', 'pt_BR', 'ru', 'tr', 'uk', 'zh_Hant_HK']
+const langs_list = ['ar', 'de', 'el', 'en', 'eo', 'es', 'fa', 'fi', 'fr', 'it', 'ml', 'nb_NO', 'pl', 'pt_BR', 'ru', 'tr', 'uk', 'zh_HK']
--- scripts/python/pug_translator.py
+++ scripts/python/pug_translator.py
@@ -6,30 +6,47 @@ import json
 for item in os.listdir("docs/lang_json/"):
     itemName = item.replace(".json", "")
 
-    dataJson = {}
-    with open("docs/lang_json/"+item) as file:
-        dataJson = json.load(file)
+    enDataJson = {}
+    with open("docs/lang_json/en.json") as file:
+        enDataJson = json.load(file)
 
-    if dataJson != {}:
+    langDataJson = {}
+    with open("docs/lang_json/"+item) as file:
+        langDataJson = json.load(file)
+
+    dict3 = {**enDataJson, **langDataJson}
+
+    print(dict3)
+
+    if dict3 != {}:
+
+        enLangsData = {}
+        with open("lib/l10n/langs/intl_en.arb") as file:
+            enLangsData = json.load(file)
 
         langsData = {}
         with open("lib/l10n/langs/intl_"+itemName+".arb") as file:
             langsData = json.load(file)
 
-        dataJson.update(langsData)
+        allLangsData = {**enLangsData, **langsData}
+
+        dict3.update(allLangsData)
 
         newJson = {}
-        for i in dataJson:
+        for i in dict3:
             if not i.startswith("@"):
-                newJson[i] = dataJson[i]
+                newJson[i] = dict3[i]
 
         # Take missing strings from main.arb
+        enData = {}
+        with open("lib/l10n/main/intl_en.arb") as file:
+            enData = json.load(file)
+        newJson.update(enData)
+
         mainData = {}
         with open("lib/l10n/main/intl_"+itemName+".arb") as file:
             mainData = json.load(file)
-
         newJson.update(mainData)
-
         data = json.dumps(newJson, ensure_ascii=False, indent=4).split('\n')
 
         for i in range(0, len(data)):
